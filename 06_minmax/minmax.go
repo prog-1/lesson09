fabd2e85ad1cb4064f219afc5e7606eedf51ad3a
Pavel Zaichenkov <zaichenkov@gmail.com>
2021-09-29 22:16:21 +0200

    introduce a version of minmax to illustrate early returns

--- 06_minmax/minmax.go
+++ 06_minmax/minmax.go
@@ -6,19 +6,20 @@ import (
 
 func main() {
 	var min, max float64
+
 	fmt.Println("This program calculates the maximum and minimum of two numbers.")
 	fmt.Print("Enter the first number: ")
 	fmt.Scan(&min)
 	fmt.Print("Enter the second number: ")
 	fmt.Scan(&max)
 
-	if max < min {
-		min, max = max, min
-	} else if max == min {
+	if max == min {
 		fmt.Println("The numbers are equal.")
-		return
+	} else {
+		if max < min {
+			min, max = max, min
+		}
+		fmt.Println("Maximum =", max)
+		fmt.Println("Minimum =", min)
 	}
-
-	fmt.Println("Maximum =", max)
-	fmt.Println("Minimum =", min)
 }
