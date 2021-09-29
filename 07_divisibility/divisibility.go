e28dd9419decc1b5846615cf2a48a56ac9a00278
Pavel Zaichenkov <zaichenkov@gmail.com>
2021-09-29 22:05:04 +0200

    small changes to divisibility program

--- 07_divisibility/divisibility.go
+++ 07_divisibility/divisibility.go
@@ -4,8 +4,6 @@ import "fmt"
 
 func main() {
 	var dividend, divisor int
-	var remainder int
-
 	fmt.Println("This program checks if one integer is divisible by another integer.")
 	fmt.Print("Enter a dividend: ")
 	fmt.Scan(&dividend)
@@ -15,14 +13,13 @@ func main() {
 	if divisor == 0 {
 		fmt.Println(" Division by zero is an invalid operation :( ")
 	} else {
-		remainder = dividend % divisor
+		remainder := dividend % divisor
 		if remainder == 0 {
 			fmt.Println(dividend, "is divisible by", divisor)
 			fmt.Println(dividend, ":", divisor, "=", dividend/divisor)
 		} else { // is not divisible (the remainder is not equal to 0)
 			fmt.Println(dividend, "is not divisible by", divisor)
-			fmt.Println(dividend, ":", divisor, "=", dividend/divisor,
-				" and", remainder, "is a remainder")
+			fmt.Println(dividend, ":", divisor, "=", dividend/divisor, "and", remainder, "is a remainder")
 		}
 	}
 }
